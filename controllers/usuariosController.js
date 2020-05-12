72f5bfa3f28a37c276608e83bb434437617922f1
CesarHeredero <heredero.cesar@gmail.com>
2020-05-12 13:31:20 +0200

    probar en mac

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,5 +1,34 @@
+const Usuarios = require('../models/Usuarios');
+
 exports.formCrearCuenta = (req, res) => {
     res.render('crear-cuenta', {
-        nombrePagina: 'Nueva Cuenta'
+        nombrePagina: 'Crea tu Cuenta'
     });
+};
+
+exports.crearNuevaCuenta = async(req, res) => {
+    const usuario = req.body;
+
+    req.checkBody('confirmar', 'La repeticion de contraseña no puede ir vacia').notEmpty();
+    req.checkBody('confirmar', 'La contraseña no coincide').equals(req.body.password);
+
+    // leer errores express
+    const erroresExpress = req.validationErrors();
+    console.log(erroresExpress);
+
+    try {
+        const nuevoUsuario = await Usuarios.create(usuario);
+        console.log('Usuario creado', nuevoUsuario);
+    } catch (error) {
+        const erroresSequelize = error.errors.map(err => err.message);
+        const errExp = erroresExpress.map(err => err.msg);
+
+
+
+        const listaErrores = [...erroresSequelize, ...errExp];
+        console.log(listaErrores);
+        req.flash('error', listaErrores);
+        res.redirect('/crear-cuenta');
+
+    }
 }
--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = require('./routes');
 const path = require('path');
 const expresLayout = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
 
 const db = require('./config/db');
 require('./models/Usuarios');
@@ -11,6 +16,11 @@ require('dotenv').config({ path: 'variables.env' });
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(expressValidator());
+
 app.use(expresLayout);
 app.set('view engine', 'ejs');
 
@@ -18,8 +28,20 @@ app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static('public'));
 
+app.use(cookieParser());
+
+app.use(session({
+    secret: process.env.SECRETO,
+    key: process.env.KEY,
+    resave: false,
+    seveUninitialized: false
+}));
+
+app.use(flash());
+
 // Middlewere (usuario logueado, flash messages, fecha actual)
 app.use((req, res, next) => {
+    res.locals.mensajes = req.flash();
     const fecha = new Date();
     res.locals.year = fecha.getFullYear();
 
